3455249f03095e0bf0658a5d18f314e3f4973be1
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-12-31 16:08:26 -0300

    :hammer: Adicionando o componente do conteúdo

--- src/components/main/App.tsx
+++ src/components/main/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components-secondary/Header";
 import Calculadora from "./components-secondary/Calculadora";
+import Conteudo from "./components-secondary/Conteudo";
 
 function App() {
   return (
     <>
       <Header />
       <Calculadora />
+      <Conteudo />
     </>
   );
 }
---
+++ src/components/main/components-secondary/Conteudo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../components-styles/Conteudo.module.css";
+
+export default function Conteudo() {
+  return (
+    <div className={style.containerContent}>
+      <div className={style.content}>
+        <p>Início do contéudo</p>
+      </div>
+    </div>
+  );
+}
