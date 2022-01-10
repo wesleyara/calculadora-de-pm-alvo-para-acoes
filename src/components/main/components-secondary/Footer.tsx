7860e7fdce997b99e023faead995f9aead1cd0d7
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-10 08:38:40 -0300

    :hammer: Alterando o componente Footer

--- src/components/main/components-secondary/Footer.tsx
+++ src/components/main/components-secondary/Footer.tsx
@@ -4,6 +4,12 @@ import style from "../components-styles/Footer.module.css";
 export default function Footer() {
   return (
     <div className={style.footer}>
+      <p className={style.reserv}>
+        <a className={style.copyright} href="/politica-de-privacidade">
+          Políticas de Privacidade
+        </a>
+      </p>
+      <br />
       <a className={style.copyright} href="mailto:wesleyara.contato@gmail.com">
         <p>Desenvolvido por Wesley Araújo</p>
       </a>
