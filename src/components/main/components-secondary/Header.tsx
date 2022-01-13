80777673fc9a430507b936d5f8519f75060abfc4
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-13 15:38:16 -0300

    :wrench: Alterando o componente Header

--- src/components/main/components-secondary/Header.tsx
+++ src/components/main/components-secondary/Header.tsx
@@ -3,12 +3,14 @@ import style from "../components-styles/Header.module.css";
 
 export default function Header() {
   return (
-    <div className={style.header}>
-      <h1>CALCULADORA DE PREÇO MÉDIO ALVO</h1>
-      <h3 className={style.sub}>
-        Calcule a quantidade de ativos necessários para alcançar seu preço médio
-        alvo!
-      </h3>
-    </div>
+    <>
+      <div id="header" className={style.header}>
+        <h1>CALCULADORA DE PREÇO MÉDIO ALVO</h1>
+        <h3 className={style.sub}>
+          Calcule a quantidade de ativos necessários para alcançar seu preço
+          médio alvo!
+        </h3>
+      </div>
+    </>
   );
 }
